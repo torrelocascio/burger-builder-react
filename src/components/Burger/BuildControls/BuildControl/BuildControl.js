a30e9cf755480c6e605b2c9f4ff9e3ea4a3de761
Torre <torre.locascio@gmail.com>
2018-04-09 14:35:59 -0400

    burger layout some functionality for adding ingredients

---
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import classes from './BuildControl.css'
+
+
+const buildControl = (props) => (
+  <div className={classes.BuildControl}>
+    <div className={classes.Label}> {props.label} </div>
+    <button className={classes.Less}>Less</button>
+    <button classname={classes.More}>More</button>
+  </div>
+
+
+) 
+
+
+
+
+export default buildControl
---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import classes from './BuildControls.css';
+import BuildControl from './BuildControl/BuildControl';
+
+const controls = [
+  {label: 'Salad', type: 'salad'},
+  {label: 'Salad', type: 'salad'},
+  {label: 'Salad', type: 'salad'},
+  {label: 'Salad', type: 'salad'},
+]
+
+const buildControls = (props) => (
+  <div className={classes.BuildControls}>
+    {controls.map(ctrl =>(
+<BuildControl key={ctrl.label} label={ctrl.label}/>
+
+    ))}
+
+  </div>
+)
